9b64a33ab20f3b4ea3951e7635b064566428c654
Mark Agib <80441909+MarkAppprogrammer@users.noreply.github.com>
2024-08-16 23:10:15 -0700

    Fixed input shape bug
    Note: fix printing out the entire model info (mask and shape)

--- typedmodel/cnn.py
+++ typedmodel/cnn.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
+tf.get_logger().setLevel('ERROR')
 
 class ResNetBlock(tf.keras.layers.Layer):
-    def __init__(self, out_channels, stride=1, downsample=False):
+    def __init__(self, out_channels, stride, downsample):
+        super().__init__()
         self.downsample = downsample
         
         self.conv1 = tf.keras.layers.Conv2D(
@@ -38,7 +40,6 @@ class ResNetBlock(tf.keras.layers.Layer):
 
     def call(self, inputs):
         shortcut = self.downsample_layer(inputs)
-        
         x = self.conv1(inputs)
         x = self.b1(x)
         x = self.relu(x)
@@ -53,14 +54,19 @@ class ResNetBlock(tf.keras.layers.Layer):
 
 class ResNetModel(tf.keras.Model):
     def __init__(self):
+        tf.get_logger().setLevel('ERROR')
+        super(ResNetModel, self).__init__()
+
         self.begginer = tf.keras.Sequential([
+            tf.keras.layers.InputLayer(
+                shape=(250, 500, 2)
+            ),
             tf.keras.layers.Conv2D(
                 filters=64,
                 kernel_size=(7, 7),
                 padding="same",
                 strides=(2, 2),
-                use_bias=False,
-                input_shape=(250, 500, 2)
+                use_bias=False
             ),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Activation('relu'),
@@ -71,11 +77,12 @@ class ResNetModel(tf.keras.Model):
             )
         ])
         
-        self.block1 = ResNetBlock(64)
+        self.block1 = ResNetBlock(64, stride=1, downsample=False)
         self.block2 = ResNetBlock(128, stride=2, downsample=True)
         self.block3 = ResNetBlock(256, stride=2, downsample=True)
         
     def call(self, inputs):
+        tf.get_logger().setLevel('ERROR')
         x = self.begginer(inputs)
         x = self.block1(x)
         x = self.block2(x)
@@ -85,7 +92,7 @@ class ResNetModel(tf.keras.Model):
     def model_config():
         model = ResNetModel()
         model.build((250, 500, 2))  
-        model.summary()
+        #model.summary()
 
         return model
 
